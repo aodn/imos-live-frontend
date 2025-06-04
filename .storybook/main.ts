e1776ab74eae1d941e913431a311c637f2f638e9
Zhonghui Duan <Zhonghui.Duan@utas.edu.au>
2025-06-04 14:03:45 +1000

    update storybook.yml for cicd

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import path from 'path';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -13,14 +14,17 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
-  viteFinal: async config => {
-    // Inject your alias from vite.config manually
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname, '../src'),
-    };
-    return config;
+  async viteFinal(config) {
+    // Merge custom configuration into the default Vite config
+    return mergeConfig(config, {
+      // Add your own Vite options here
+      resolve: {
+        alias: {
+          // Example: if your tsconfig.json has paths: { "@/*": ["src/*"] }
+          '@': path.resolve(__dirname, '../src'), // Adjust path as needed
+        },
+      },
+    });
   },
 };
 export default config;
