ea54111c2757d1f665b3e5fef91052ee97cc6c94
Zhonghui Duan <Zhonghui.Duan@utas.edu.au>
2025-07-09 10:29:36 +1000

    fix: enable layereindicator to close layer

--- src/components/LayersIndicator/LayersIndicator.tsx
+++ src/components/LayersIndicator/LayersIndicator.tsx
@@ -2,6 +2,7 @@ import { MapUIState, useMapUIStore } from '@/store';
 import { cn } from '@/utils';
 import { useShallow } from 'zustand/shallow';
 import { Button } from '../Button';
+import { CloseIcon } from '../Icons';
 
 type Label = keyof Pick<MapUIState, 'circle' | 'particles' | 'overlay'>;
 
@@ -14,13 +15,36 @@ type LayersIndicatorProps = {
 };
 
 export const LayersIndicator = ({ layers, className }: LayersIndicatorProps) => {
-  const { particles, overlay, circle } = useMapUIStore(
-    useShallow(s => ({ overlay: s.overlay, particles: s.particles, circle: s.circle })),
+  const { particles, overlay, circle, setCircle, setOverlay, setParticles } = useMapUIStore(
+    useShallow(s => ({
+      overlay: s.overlay,
+      particles: s.particles,
+      circle: s.circle,
+      setOverlay: s.setOverlay,
+      setParticles: s.setParticles,
+      setCircle: s.setCircle,
+    })),
   );
   const visibles: Record<Label, boolean> = {
     particles,
     overlay,
     circle,
+  };
+
+  const handleClose = (label: Label) => () => {
+    switch (label) {
+      case 'circle':
+        setCircle(false);
+        break;
+      case 'overlay':
+        setOverlay(false);
+        break;
+      case 'particles':
+        setParticles(false);
+        break;
+      default:
+        break;
+    }
   };
 
   return (
@@ -30,11 +54,17 @@ export const LayersIndicator = ({ layers, className }: LayersIndicatorProps) => 
           <Button
             key={'LayersIndicator-' + label}
             aria-label={label}
-            className={cn('h-10 w-10 rounded bg-white flex justify-center items-center', {
+            className={cn('relative h-10 w-10 rounded bg-white flex justify-center items-center', {
               hidden: !visibles[label],
             })}
+            onClick={handleClose(label)}
           >
             <Icon size="lg" />
+            <CloseIcon
+              size="xs"
+              color="imos-red"
+              className="absolute right-0 top-0 border rounded-4xl"
+            />
           </Button>
         );
       })}
