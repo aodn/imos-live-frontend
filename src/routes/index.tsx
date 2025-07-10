d2d62a79bf219209af8d0afae94a29ac615cc3a3
gtrrz-victor <gtrrz.victor@gmail.com>
2025-07-10 16:07:26 +1000

    feature: redirects to / if route not found (#4)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,14 @@
-import { Map as MapPage, Landing as LandingPage } from '@/pages';
+import { Map as MapPage } from '@/pages';
+import { Navigate } from 'react-router-dom';
 
 export const routes = [
   { path: '/', name: 'map', element: <MapPage /> },
-  { path: '/landing', name: 'landing', element: <LandingPage /> },
+  { path: '*', name: 'not-found', element: <Navigate to="/" replace /> },
 ] as const;
 
 export type RouteConfig = (typeof routes)[number];
 export type PageName = RouteConfig['name'];
 export type PathName = RouteConfig['path'];
-
-/**
-{
-    "/": "map",
-    "/landing": "landing"
-}
- */
 export const routeMap = Object.fromEntries(routes.map(({ path, name }) => [path, name])) as Record<
   PathName,
   PageName
