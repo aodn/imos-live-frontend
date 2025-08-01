bbeb00ccca3fa2e3e48f27bc6edcbc8eee8ff49d
Zhonghui Duan <Zhonghui.Duan@utas.edu.au>
2025-08-01 17:07:14 +1000

    fix: add ESRIWorldImagery layer and udpate defualt layers to added

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,8 +1,11 @@
 import terrain from './terrain.json';
+import ESRIWorldImagery from './ESRIWorldImagery.json';
 
 type MapboxStyle = { title: 'Dark' | 'Streets' | 'Satellite'; source: string };
-type CustomStyle = { title: 'Terrain'; source: typeof terrain };
-export type Style = MapboxStyle | CustomStyle;
+type TerrianStyle = { title: 'Terrain'; source: typeof terrain };
+type ESRIWorldImageryStyle = { title: 'ESRIWorldImagery'; source: typeof ESRIWorldImagery };
+
+export type Style = MapboxStyle | TerrianStyle | ESRIWorldImageryStyle;
 
 const mapboxStyles: MapboxStyle[] = [
   { title: 'Dark', source: 'mapbox://styles/mapbox/dark-v11' },
@@ -10,13 +13,17 @@ const mapboxStyles: MapboxStyle[] = [
   { title: 'Satellite', source: 'mapbox://styles/mapbox/satellite-v9' },
 ];
 
-export const customStyles: CustomStyle[] = [{ title: 'Terrain', source: terrain }];
+export const customStyles: (TerrianStyle | ESRIWorldImageryStyle)[] = [
+  { title: 'Terrain', source: terrain },
+  { title: 'ESRIWorldImagery', source: ESRIWorldImagery },
+];
 
 export const styles: Style[] = [...mapboxStyles, ...customStyles];
 
-export type StyleTitle = 'Dark' | 'Streets' | 'Satellite' | 'Terrain';
+export type StyleTitle = 'Dark' | 'Streets' | 'Satellite' | 'Terrain' | 'ESRIWorldImagery';
 export type StyleSource =
   | 'mapbox://styles/mapbox/dark-v11'
   | 'mapbox://styles/mapbox/streets-v12'
   | 'mapbox://styles/mapbox/satellite-v9'
-  | typeof terrain;
+  | typeof terrain
+  | typeof ESRIWorldImagery;
