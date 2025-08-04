b2623fb5a6235b5a2c43a63b0fa15952f9b8315a
Victor Gutierrez <victor.gutierrezcalderon@utas.edu.au>
2025-08-04 12:58:49 +1000

    chore: add waves buoys test

--- tests/happyPath.spec.ts
+++ tests/happyPath.spec.ts
@@ -1,7 +1,13 @@
-import { OVERLAY_LAYER_ID, PARTICLE_LAYER_ID } from '@/constants/map';
+import {
+  OVERLAY_LAYER_ID,
+  PARTICLE_LAYER_ID,
+  UNCLUSTERED_WAVE_BUOYS_LAYER_ID,
+  WAVE_BUOYS_LAYER_ID,
+} from '@/constants/map';
 import { VectoryLayerInterface } from '@/layers';
 import { expect, Page, test } from '@playwright/test';
 import { type Map } from 'mapbox-gl';
+import { genBuoyData } from '../test-data/buoy';
 import { genData } from '../test-data/gsla';
 
 type PopupContent = Partial<{
@@ -48,7 +54,8 @@ const mapComponent = {
   },
 };
 
-type Product = 'GSLA Ocean current product' | 'GSLA Anomaly sea levels';
+type LngLat = [number, number];
+type Product = 'GSLA Ocean current product' | 'GSLA Anomaly sea levels' | 'Wave buoys product';
 
 const sidebarComponent = {
   selectProduct: async (page: Page, productName: Product) => {
@@ -78,7 +85,7 @@ test.describe('Ocean Current', () => {
     await sidebarComponent.selectProduct(page, 'GSLA Ocean current product');
   });
 
-  test('User can see the current value from a map particle', async ({ page }) => {
+  test('User can see the current value from a map particle of different days', async ({ page }) => {
     await mapComponent.waitUntilLayerLoaded(page, PARTICLE_LAYER_ID);
     await mapComponent.expectPopupToHaveContent(page, {
       speed: '1.00 m/s',
@@ -106,7 +113,7 @@ test.describe('Anomaly sea levels', () => {
     await sidebarComponent.selectProduct(page, 'GSLA Anomaly sea levels');
   });
 
-  test('User can see the current value from a map particle', async ({ page }) => {
+  test('User can see the current value from a map particle of different days', async ({ page }) => {
     await mapComponent.waitUntilLayerLoaded(page, OVERLAY_LAYER_ID);
     await mapComponent.expectPopupToHaveContent(page, {
       gsla: '3.00 m',
@@ -131,7 +138,7 @@ test.describe('Anomaly sea levels and Ocean Current', () => {
     await sidebarComponent.selectProduct(page, 'GSLA Ocean current product');
   });
 
-  test('User can see the current value from a map particle', async ({ page }) => {
+  test('User can see the current value from a map particle of different days', async ({ page }) => {
     await mapComponent.waitUntilLayerLoaded(page, OVERLAY_LAYER_ID);
     await mapComponent.expectPopupToHaveContent(page, {
       gsla: '3.00 m',
@@ -152,5 +159,158 @@ test.describe('Anomaly sea levels and Ocean Current', () => {
       direction: 'E',
       bearing: '3.00°',
     });
+  });
+});
+
+test.describe('Wave Buoys', () => {
+  const buoys = {
+    HOBARITO: {
+      name: 'HOBARITO',
+      coordinates: [147.33, -42.88] satisfies LngLat,
+    },
+    DARWIN: {
+      name: 'DARWIN',
+      coordinates: [130.78, -12.1] satisfies LngLat,
+    },
+  };
+  test.beforeEach(async ({ page }) => {
+    await page.route('*/**/BUOY/buoy_locations/buoy_locations_2025-07-23.geojson', async route => {
+      const buoyLocations = {
+        type: 'FeatureCollection',
+        metadata: {},
+        features: [
+          {
+            type: 'Feature',
+            properties: {
+              date: '2025-07-23',
+              buoy: buoys.HOBARITO.name,
+              year: 2025,
+              timestamp: '2025-07-23T00:10:00',
+            },
+            geometry: {
+              coordinates: buoys.HOBARITO.coordinates,
+              type: 'Point',
+            },
+          },
+        ],
+      };
+      await route.fulfill({ json: buoyLocations });
+    });
+
+    await page.route('*/**/BUOY/buoy_locations/buoy_locations_2025-07-24.geojson', async route => {
+      const buoyLocations = {
+        type: 'FeatureCollection',
+        metadata: {},
+        features: [
+          {
+            type: 'Feature',
+            properties: {
+              date: '2025-07-24',
+              buoy: buoys.DARWIN.name,
+              year: 2025,
+              timestamp: '2025-07-24T00:10:00',
+            },
+            geometry: {
+              coordinates: buoys.DARWIN.coordinates,
+              type: 'Point',
+            },
+          },
+        ],
+      };
+      await route.fulfill({ json: buoyLocations });
+    });
+
+    await page.route('*/**/BUOY/buoy_details/*.geojson', async route => {
+      const dateMatch = route
+        .request()
+        .url()
+        .match(/BUOY\/buoy_details\/([^_]+)_(\d{4}-\d{2}-\d{2})\.geojson/);
+      if (dateMatch) {
+        const buoyName = dateMatch[1];
+        const dataDate = new Date(dateMatch[2]);
+        await route.fulfill({
+          json: genBuoyData(
+            { name: buoyName, dataDate },
+            {
+              sswmd: (date: Date) => {
+                const dateTime = new Date(date);
+                dateTime.setHours(0, 0, 0, 0);
+                return [[dateTime.getTime(), 180]];
+              },
+              wpfm: (date: Date) => {
+                const dateTime = new Date(date);
+                dateTime.setHours(0, 0, 0, 0);
+                return [[dateTime.getTime(), 40]];
+              },
+              wssh: (date: Date) => {
+                const dateTime = new Date(date);
+                dateTime.setHours(0, 0, 0, 0);
+                return [[dateTime.getTime(), 80]];
+              },
+            },
+          ),
+        });
+      }
+    });
+
+    await page.goto('/');
+    await sidebarComponent.selectProduct(page, 'Wave buoys product');
+  });
+
+  test('User can see the see the bouy`s historical by clicking at the buoy', async ({ page }) => {
+    await mapComponent.waitUntilLayerLoaded(page, WAVE_BUOYS_LAYER_ID);
+
+    const point = await page.evaluate(layer => {
+      const map = (window as any).map as Map | undefined;
+      if (!map) throw new Error('Map not found');
+
+      const features = map?.queryRenderedFeatures(undefined as any, { layers: [layer] });
+      if (!features || features.length === 0) throw new Error('No features found');
+      const [buoy] = features;
+
+      const coordinates = buoy.geometry.type === 'Point' && buoy.geometry.coordinates;
+      if (!coordinates) throw new Error('Coordinates not found');
+      if (coordinates.length !== 2) throw new Error('Invalid coordinates length');
+
+      return map.project(coordinates as LngLat);
+    }, UNCLUSTERED_WAVE_BUOYS_LAYER_ID);
+
+    await page.getByRole('region', { name: 'Map' }).click({ position: { x: point.x, y: point.y } });
+    await expect(
+      page.getByLabel('Interactive chart', { exact: true }).getByText('HOBARITO'),
+    ).toBeVisible();
+  });
+
+  test('User can see the see the bouys location of different days', async ({ page }) => {
+    await mapComponent.waitUntilLayerLoaded(page, WAVE_BUOYS_LAYER_ID);
+
+    await page.waitForFunction(
+      ({ layer, expectedBuoyName }) => {
+        const map = (window as any).map as Map | undefined;
+        if (!map) throw new Error('Map not found');
+
+        return map?.queryRenderedFeatures(undefined as any, { layers: [layer] }).some(feature => {
+          return feature.properties?.buoy === expectedBuoyName;
+        });
+      },
+      { layer: UNCLUSTERED_WAVE_BUOYS_LAYER_ID, expectedBuoyName: buoys.HOBARITO.name },
+    );
+
+    await page.getByRole('slider', { name: 'point handle' }).click();
+    await page.keyboard.press('ArrowRight');
+
+    await page.waitForFunction(
+      ({ layer, expectedBuoyName }) => {
+        const map = (window as any).map as Map | undefined;
+        if (!map) throw new Error('Map not found');
+
+        return map?.queryRenderedFeatures(undefined as any, { layers: [layer] }).some(feature => {
+          return feature.properties?.buoy === expectedBuoyName;
+        });
+      },
+      { layer: UNCLUSTERED_WAVE_BUOYS_LAYER_ID, expectedBuoyName: buoys.DARWIN.name },
+    );
+
+    await page.waitForTimeout(5000);
   });
 });
