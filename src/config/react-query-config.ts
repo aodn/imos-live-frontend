43b795183b461819625a949186795e2e817bc128
Zhonghui Duan <Zhonghui.Duan@utas.edu.au>
2025-08-04 18:28:29 +1000

    chore: update cacheConfig method

--- src/config/react-query-config.ts
+++ src/config/react-query-config.ts
@@ -2,12 +2,13 @@ import { getDate3DaysAgo, isSameDay } from '@/utils';
 
 /**
  * if dataset is 3 days ago, then disable cache, otherwise ebable cache.
- * @param dataset expted to be as 'yyyy--mm--dd'
+ * @param dataset expted to be as 'yyyy--mm--dd'.
+ * @param duration expected to be minute unit.
  * @returns
  */
-export function cacheConfig(dataset: string) {
+export function cacheConfig(dataset: string, duration?: number) {
   return {
-    gcTime: isSameDay(getDate3DaysAgo(), new Date(dataset)) ? 0 : 60 * 60 * 1000,
-    staleTime: isSameDay(getDate3DaysAgo(), new Date(dataset)) ? 0 : 60 * 60 * 1000,
+    gcTime: isSameDay(getDate3DaysAgo(), new Date(dataset)) ? (duration ?? 60) : 60 * 60 * 1000,
+    staleTime: isSameDay(getDate3DaysAgo(), new Date(dataset)) ? (duration ?? 60) : 60 * 60 * 1000,
   };
 }
