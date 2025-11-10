1830daa22e3541d7df061f8fe7f4b2f50724ded5
Victor Gutierrez Calderon <victor@codezero.io>
2025-11-10 14:47:31 +1100

    feature: timepicker last 7 days since today

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,63 +1,26 @@
 import { FixedLengthArray } from '@/types';
 
 /**
- * Get the last 7 dates in the format "YYYY-MM-DD" ending 3 days ago.
- * The dates are in descending order, starting from 6 days ago to 3 days ago.
- * For example, if today is 2023-10-10, the output will be:
+ * Get the last 7 dates in the format "YYYY-MM-DD".
+ * The dates are in descending order, starting from 6 days ago.
+ * For example, if today is 2023-10-07, the output will be:
  * ["2023-10-01", "2023-10-02", "2023-10-03", "2023-10-04", "2023-10-05", "2023-10-06", "2023-10-07"]
- * @returns Last 7 dates in the format "YY-MM-DD" ending 3 days ago.
+ * @returns Last 7 dates in the format "YY-MM-DD".
  * By passing format like 'yyyy-mm-dd', 'yy-mm-dd', 'dd/mm/yyyy', it will generate dates liek:
- * getLast7DatesEnding3DaysAgo('yy-mm-dd');
+ * getLast7Dates('yy-mm-dd');
     → ['24-05-25', ..., '24-05-31']
 
-    getLast7DatesEnding3DaysAgo('yyyy/mm/dd');
+    getLast7Dates('yyyy/mm/dd');
     → ['2024/05/25', ..., '2024/05/31']
 
-    getLast7DatesEnding3DaysAgo('dd.mm.yyyy');
+    getLast7Dates('dd.mm.yyyy');
     → ['25.05.2024', ..., '31.05.2024']
  */
-export function getLast7DatesEnding3DaysAgo(
-  format: string = 'yyyy-mm-dd',
-): FixedLengthArray<string, 7> {
+export function getLast7Dates(format: string = 'yyyy-mm-dd'): FixedLengthArray<string, 7> {
   const dates: string[] = [];
   const today = new Date();
   const endDate = new Date(today);
-  endDate.setDate(today.getDate() - 3);
-
-  for (let i = 6; i >= 0; i--) {
-    const date = new Date(endDate);
-    date.setDate(endDate.getDate() - i);
-
-    const yyyy = date.getFullYear();
-    const yy = String(yyyy).slice(-2);
-    const mm = String(date.getMonth() + 1).padStart(2, '0');
-    const dd = String(date.getDate()).padStart(2, '0');
-
-    const formattedDate = format
-      .replace(/yyyy/g, String(yyyy))
-      .replace(/yy/g, yy)
-      .replace(/mm/g, mm)
-      .replace(/dd/g, dd);
-
-    dates.push(formattedDate);
-  }
-
-  return dates as FixedLengthArray<string, 7>;
-}
-
-/**
- *
- * @param date string in 'yyyy-mm-dd' format
- * @param format
- * @returns  Last 7 dates in the format "YY-MM-DD" ending provided date.
- */
-export function getLast7Dates(
-  date: string,
-  format: string = 'yyyy-mm-dd',
-): FixedLengthArray<string, 7> {
-  const dates: string[] = [];
-
-  const endDate = new Date(date);
+  endDate.setDate(today.getDate());
 
   for (let i = 6; i >= 0; i--) {
     const date = new Date(endDate);
