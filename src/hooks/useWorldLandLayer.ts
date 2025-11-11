e45ab41f800b9523e84e8493487d2da03d84777a
Zhonghui Duan <Zhonghui.Duan@utas.edu.au>
2025-11-12 10:37:34 +1100

    fix: remove terrian and remove land fill layer

--- src/hooks/useWorldLandLayer.ts
+++ src/hooks/useWorldLandLayer.ts
@@ -1,13 +1,9 @@
-import {
-  WORLD_LAND_BORDER_LAYER_ID,
-  WORLD_LAND_FILL_LAYER_ID,
-  WORLD_LAND_SOURCE_ID,
-} from '@/constants';
+import { WORLD_LAND_BORDER_LAYER_ID, WORLD_LAND_SOURCE_ID } from '@/constants';
 import { useCallback, useMemo } from 'react';
 import { useMapboxLayerSetup } from './useMapboxLayerSetup';
-import { fillLayer, lineLayer } from '@/layers';
+import { lineLayer } from '@/layers';
 import { addLayerInOrder, addOrUpdateVectorSource } from '@/helpers';
-import { worldLandBorderConfig, worldLandFillConfig } from '@/config';
+import { worldLandBorderConfig } from '@/config';
 import { worldLandStyle } from '@/styles';
 import { useMapUIStore } from '@/store';
 import { useShallow } from 'zustand/shallow';
@@ -18,18 +14,6 @@ export function useWorldLandLayer(map: React.RefObject<mapboxgl.Map | null>) {
     useShallow(s => ({
       worldBoundaries: s.worldBoundaries,
     })),
-  );
-  const worldLandFillLayer = useMemo(
-    () =>
-      fillLayer(
-        {
-          id: WORLD_LAND_FILL_LAYER_ID,
-          source: WORLD_LAND_SOURCE_ID,
-          ...worldLandFillConfig,
-        },
-        worldBoundaries,
-      ),
-    [worldBoundaries],
   );
 
   const worldLandBorderLayer = useMemo(
@@ -45,11 +29,6 @@ export function useWorldLandLayer(map: React.RefObject<mapboxgl.Map | null>) {
     [worldBoundaries],
   );
 
-  const landLayers = useMemo(
-    () => [worldLandFillLayer, worldLandBorderLayer],
-    [worldLandFillLayer, worldLandBorderLayer],
-  );
-
   const setupLayer = useCallback(async () => {
     if (!map.current) return;
     addOrUpdateVectorSource({
@@ -57,15 +36,10 @@ export function useWorldLandLayer(map: React.RefObject<mapboxgl.Map | null>) {
       id: WORLD_LAND_SOURCE_ID,
       url: worldLandStyle.source,
     });
-    landLayers.forEach(layer => addLayerInOrder(map, layer));
-  }, [landLayers, map]);
+    addLayerInOrder(map, worldLandBorderLayer);
+  }, [worldLandBorderLayer, map]);
 
   const { loadComplete } = useMapboxLayerSetup(map, setupLayer, []);
 
-  useMapboxLayerVisibility(
-    map,
-    loadComplete,
-    [worldLandBorderLayer, worldLandFillLayer],
-    worldBoundaries,
-  );
+  useMapboxLayerVisibility(map, loadComplete, [worldLandBorderLayer], worldBoundaries);
 }
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,11 +1,9 @@
-import terrain from './terrain.json';
 import ESRIWorldImagery from './ESRIWorldImagery.json';
 
 type MapboxStyle = { title: 'Dark' | 'Streets' | 'Satellite'; source: string };
-type TerrianStyle = { title: 'Terrain'; source: typeof terrain };
 type ESRIWorldImageryStyle = { title: 'ESRIWorldImagery'; source: typeof ESRIWorldImagery };
 
-export type Style = MapboxStyle | TerrianStyle | ESRIWorldImageryStyle;
+export type Style = MapboxStyle | ESRIWorldImageryStyle;
 
 const mapboxStyles: MapboxStyle[] = [
   { title: 'Dark', source: 'mapbox://styles/mapbox/dark-v11' },
@@ -13,19 +11,17 @@ const mapboxStyles: MapboxStyle[] = [
   { title: 'Satellite', source: 'mapbox://styles/mapbox/satellite-v9' },
 ];
 
-export const customStyles: (TerrianStyle | ESRIWorldImageryStyle)[] = [
-  { title: 'Terrain', source: terrain },
+export const customStyles: ESRIWorldImageryStyle[] = [
   { title: 'ESRIWorldImagery', source: ESRIWorldImagery },
 ];
 
 export const styles: Style[] = [...mapboxStyles, ...customStyles];
 
-export type StyleTitle = 'Dark' | 'Streets' | 'Satellite' | 'Terrain' | 'ESRIWorldImagery';
+export type StyleTitle = 'Dark' | 'Streets' | 'Satellite' | 'ESRIWorldImagery';
 export type StyleSource =
   | 'mapbox://styles/mapbox/dark-v11'
   | 'mapbox://styles/mapbox/streets-v12'
   | 'mapbox://styles/mapbox/satellite-v9'
-  | typeof terrain
   | typeof ESRIWorldImagery;
 
 export const worldLandStyle = {
