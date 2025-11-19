5b61528dcecde4a042eed02b8a50c1ef93c22465
Zhonghui Duan <Zhonghui.Duan@utas.edu.au>
2025-11-20 10:44:44 +1100

    fix: stop breaking buoy chart when no buoy detail  data

--- src/components/Highcharts/WaveBuoyChart.tsx
+++ src/components/Highcharts/WaveBuoyChart.tsx
@@ -235,7 +235,7 @@ const WaveBuoyChart = ({ waveBuoysData, showDirection }: WaveBuoyChartProps) => 
           customFormatter: tooltipFormatter,
         }}
       />
-      <LatestObservation feature={feature} />
+      {feature && feature.properties && <LatestObservation feature={feature} />}
     </div>
   );
 };
