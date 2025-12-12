4c69a24f16d65b38c759445a95f4c330f56cf95f
leslie <leslie@leslies-MacBook-Pro.local>
2025-12-12 11:42:17 +1100

    fix: add shadow below MapControlPanel buttons

--- src/components/MapControlPanel/MapControlPanel.tsx
+++ src/components/MapControlPanel/MapControlPanel.tsx
@@ -22,12 +22,26 @@ export const MapControlPanel = ({
   const handlePan = () => {};
 
   return (
-    <div className={cn('flex flex-col items-center gap-2 p-4  bg-transparent', className)}>
-      <Button variant="ghost" size="icon" aria-label="Zoom in" onClick={handleZoomIn}>
-        <AddCircleIcon size="xl" color="imos-white" />
+    <div className={cn('flex flex-col items-center gap-3 p-4', className)}>
+      <Button
+        variant="ghost"
+        size="icon"
+        aria-label="Zoom in"
+        onClick={handleZoomIn}
+        className="hover:bg-transparent hover:text-imos-white text-imos-white hover:scale-110 w-9 h-9 drop-shadow-[0_0_3px_rgba(0,0,0,1)]"
+        asChild
+      >
+        <AddCircleIcon size="xl" />
       </Button>
-      <Button variant="ghost" size="icon" aria-label="Zoom out" onClick={handleZoomOut}>
-        <MinusCircleIcon size="xl" color="imos-white" />
+      <Button
+        variant="ghost"
+        size="icon"
+        aria-label="Zoom out"
+        onClick={handleZoomOut}
+        className="hover:bg-transparent hover:text-imos-white text-imos-white hover:scale-110 w-9 h-9 drop-shadow-[0_0_3px_rgba(0,0,0,1)]"
+        asChild
+      >
+        <MinusCircleIcon size="xl" />
       </Button>
       <Button
         variant="ghost"
@@ -35,8 +49,10 @@ export const MapControlPanel = ({
         aria-label="Pan"
         onClick={handlePan}
         isActive={isPanActive}
+        className="hover:bg-transparent hover:text-imos-white text-imos-white hover:scale-110 w-9 h-9 drop-shadow-[0_0_3px_rgba(0,0,0,1)]"
+        asChild
       >
-        <HandIcon size="xl" color="imos-white" />
+        <HandIcon />
       </Button>
     </div>
   );
