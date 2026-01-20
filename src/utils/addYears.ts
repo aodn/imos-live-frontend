919f61d503dc4a121f2857934102a8c70c48d778
leslieduan <zhonghui.duan@utas.edu.au>
2026-01-20 17:07:57 +1100

    fix: sst intial load error

--- src/utils/addYears.ts
+++ src/utils/addYears.ts
@@ -3,3 +3,9 @@ export function addYears(date: Date, years: number): Date {
   d.setFullYear(d.getFullYear() + years);
   return d;
 }
+
+export function minusYears(date: Date, years: number): Date {
+  const d = new Date(date.getTime());
+  d.setFullYear(d.getFullYear() - years);
+  return d;
+}
