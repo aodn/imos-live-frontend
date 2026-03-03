5558774ab59d6c324fb2b38887ac9a387c5be837
leslieduan <zhonghui.duan@utas.edu.au>
2026-03-03 11:13:29 +1100

    fix: fail to get latest gsla date (#213)

--- src/api/fileExist.ts
+++ src/api/fileExist.ts
@@ -2,22 +2,28 @@ import axios from 'axios';
 
 export const sstaUrl = (date: string) =>
   `https://imos-data.s3.ap-southeast-2.amazonaws.com/IMOS/SRS/AusTemp/ssta/${date.slice(0, 4)}/${date}_IMOS_AusTemp-sst-anomaly_AUS_fv02.nc`;
-export const gslaUrl = (date: string) =>
-  `https://imos-data.s3.ap-southeast-2.amazonaws.com/IMOS/OceanCurrent/GSLA/NRT/${date.slice(0, 4)}/IMOS_OceanCurrent_HV_${date}T180000Z_GSLA_FV02_NRT.nc`;
+export const gslaUrl = (date: string) => [
+  `https://imos-data.s3.ap-southeast-2.amazonaws.com/IMOS/OceanCurrent/GSLA/NRT/${date.slice(0, 4)}/IMOS_OceanCurrent_HV_${date}T180000Z_GSLA_FV02_NRT.nc`,
+  `https://imos-data.s3.ap-southeast-2.amazonaws.com/IMOS/OceanCurrent/GSLA/NRT/${date.slice(0, 4)}/IMOS_OceanCurrent_HV_${date}T000000Z_GSLA_FV02_NRT.nc`,
+];
 
-export async function fileExist(url: string, date: string): Promise<string | null> {
-  const controller = new AbortController();
-  const id = setTimeout(() => controller.abort(), 1200);
+export async function fileExist(url: string | string[], date: string): Promise<string | null> {
+  const urls = Array.isArray(url) ? url : [url];
+
+  const checks = urls.map(async u => {
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), 1200);
+    try {
+      await axios.head(u, { signal: controller.signal });
+    } finally {
+      clearTimeout(id);
+    }
+  });
 
   try {
-    await axios.head(url, {
-      signal: controller.signal,
-      //by default, validateStatus = status => status >= 200 && status < 300, so any other status will throw exception.
-    });
+    await Promise.any(checks);
     return date;
   } catch {
     return null;
-  } finally {
-    clearTimeout(id);
   }
 }
