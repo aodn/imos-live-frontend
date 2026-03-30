d2bf3ba0b13375ef20bf23e28c2a26f4f5a143ab
leslieduan <zhonghui.duan@utas.edu.au>
2026-03-30 14:37:17 +1100

    feature: enable latest date button for gsla product

--- src/components/MainSidebar/LayerCard.tsx
+++ src/components/MainSidebar/LayerCard.tsx
@@ -34,7 +34,8 @@ export function LayerCard({
   const isRasterProduct = product === 'gsla-anomaly-sea-levels' || product === 'sst-anom-mosaic';
   const isGeostrophicCurrentProduct = product === 'gsla-ocean-geostrophic-current';
   const isWaveBuoyProduct = product === 'wave-buoys';
-
+  // this is a temporary solution to get the latest available raster date. We should have a better way to get the latest date
+  // for each product in the future, like have a json file in s3 bucket that get updated when new data available.
   const { data: latestRasterDate, isLoading: isRasterDateLoading } = useQuery({
     queryKey: [product, QUERY_DATE_RANGE],
     queryFn: () => {
