c0df7ce5bb12be46e5557b7fc0bb43d74c43d092
leslieduan <zhonghui.duan@utas.edu.au>
2026-04-16 16:27:04 +1000

    fix: update ssta URL to point to Marine-Heatwave dataset

--- src/api/fileExist.ts
+++ src/api/fileExist.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const sstaUrl = (date: string) =>
-  `https://imos-data.s3.ap-southeast-2.amazonaws.com/IMOS/SRS/AusTemp/ssta/${date.slice(0, 4)}/${date}_IMOS_AusTemp-sst-anomaly_AUS_fv02.nc`;
+  `https://imos-data.s3.ap-southeast-2.amazonaws.com/IMOS/SRS/AusTemp/Marine-Heatwave/${date.slice(0, 4)}/${date}_IMOS_AusTemp-marine-heatwave_AUS_fv02.nc`;
 export const gslaUrl = (date: string) => [
   `https://imos-data.s3.ap-southeast-2.amazonaws.com/IMOS/OceanCurrent/GSLA/NRT/${date.slice(0, 4)}/IMOS_OceanCurrent_HV_${date}T180000Z_GSLA_FV02_NRT.nc`,
   `https://imos-data.s3.ap-southeast-2.amazonaws.com/IMOS/OceanCurrent/GSLA/NRT/${date.slice(0, 4)}/IMOS_OceanCurrent_HV_${date}T000000Z_GSLA_FV02_NRT.nc`,
