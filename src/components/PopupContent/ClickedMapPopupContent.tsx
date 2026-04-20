21bd361c7ac11060e2510db05a4a1e0203e3fab4
leslieduan <zhonghui.duan@utas.edu.au>
2026-04-20 10:24:37 +1000

    Fix/dhd mosaic unit (#234)

--- src/components/PopupContent/ClickedMapPopupContent.tsx
+++ src/components/PopupContent/ClickedMapPopupContent.tsx
@@ -128,9 +128,9 @@ export function ClickedMapPopupContent({
                 className="flex-col md:flex-row flex justify-between md:items-center"
                 aria-label="Sea level anomaly details"
               >
-                <span className="text-gray-600 ">DHD anomaly mosaic:</span>
+                <span className="text-gray-600 ">DHD mosaic:</span>
                 <span className="text-gray-900 ">
-                  {data[PRODUCT.AUSTEMP_DHD_MOSAIC]?.dhdAnom?.toFixed(2)} m
+                  {data[PRODUCT.AUSTEMP_DHD_MOSAIC]?.dhdAnom?.toFixed(2)} °C
                 </span>
               </div>
             )}
