590061de63ebeae0412d2585d4d770bcdb3a05ed
leslieduan <zhonghui.duan@utas.edu.au>
2026-06-11 14:18:36 +1000

    refactor: merge wave buoy types into one file

--- src/types/waveBuoysPoint.ts
+++
@@ -1,4 +0,0 @@
-import type { Point } from 'geojson';
-
-// geojson's Point allows any-length `Position`; wave buoy points are always [lng, lat].
-export type BuoyPoint = Point & { coordinates: [number, number] };
