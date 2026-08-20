5ad91182ebba61b7ab6cb9f5698e6a58031e068d
leslieduan <zhonghui.duan@utas.edu.au>
2026-08-20 17:45:42 +1000

    feature: add timezone selection

--- src/components/FeaturesMenu/MapsSection.tsx
+++ src/components/FeaturesMenu/MapsSection.tsx
@@ -1,5 +1,6 @@
 import { Dropdown } from '../Dropdown';
-import { useMapUIStore, setStyle } from '@/store';
+import type { TIMEZONE } from '@/store';
+import { useMapUIStore, setStyle, setTimezone } from '@/store';
 import { useShallow } from 'zustand/shallow';
 import type { StyleTitle } from '@/styles';
 import { styles } from '@/styles';
@@ -9,10 +10,16 @@ const STYLE_OPTIONS = styles.map(s => ({
   value: s.title,
 }));
 
+const TIMEZONE_OPTIONS: { label: string; value: TIMEZONE }[] = [
+  { label: 'UTC', value: 'UTC' },
+  { label: 'LOCAL', value: 'LOCAL' },
+];
+
 export function MapsSection() {
-  const { style } = useMapUIStore(
+  const { style, timezone } = useMapUIStore(
     useShallow(s => ({
       style: s.style,
+      timezone: s.timezone,
     })),
   );
 
@@ -20,13 +27,25 @@ export function MapsSection() {
     setStyle(value as StyleTitle);
   };
 
+  const handleTimezoneChange = (value: string | number | (string | number)[]) => {
+    setTimezone(value as 'UTC' | 'LOCAL');
+  };
+
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col gap-3">
       <Dropdown
         label="Map style"
         onChange={handleStyleChange}
         options={STYLE_OPTIONS}
         initialValue={style || STYLE_OPTIONS[0].value}
+        position="auto"
+        usePortal
+      />
+      <Dropdown
+        label="Timezone selection"
+        onChange={handleTimezoneChange}
+        options={TIMEZONE_OPTIONS}
+        initialValue={timezone || TIMEZONE_OPTIONS[0].value}
         position="auto"
         usePortal
       />
