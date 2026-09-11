75555a7c89aae4d86b26320e53c23243feda9c47
leslieduan <zhonghui.duan@utas.edu.au>
2026-09-11 11:35:22 +1000

    fix: update data gap threshold for Mooring and WaveBuoy charts to one… (#299)
    … hour

--- src/components/Highcharts/WaveBuoyChart.tsx
+++ src/components/Highcharts/WaveBuoyChart.tsx
@@ -35,7 +35,7 @@ import type Highcharts from 'highcharts/highstock';
 dayjs.extend(utc);
 
 export const WAVE_BUOY_MIN_DATE = 30;
-const WAVE_BUOY_DATA_GAP_THRESHOLD_MS = 24 * 60 * 60 * 1000;
+const WAVE_BUOY_DATA_GAP_THRESHOLD_MS = 60 * 60 * 1000;
 
 type WaveBuoyChartProps = {
   waveBuoysData: Omit<SiteFeature, 'type'>[];
